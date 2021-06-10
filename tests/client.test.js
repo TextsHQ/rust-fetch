85d3358ab3766ad374c65979f710ab8c2072e174
rumblefrog <contact@zhenyangli.me>
2021-06-09 23:15:28 -0400

    test(client) - compression, request methods, & headers

--- tests/client.test.js
+++ tests/client.test.js
@@ -1,13 +1,62 @@
 const { Builder } = require('../lib');
 
-test('Fetch JSON document', async () => {
-    let client = new Builder()
+let client;
+
+beforeAll(()  => {
+    client = new Builder()
         .setUserAgent('Glub Glub')
         .build();
+});
 
+test('Fetch JSON document', async () => {
     let ret = await client.request('https://httpbin.org/json');
 
     expect(ret.status).toBe(200);
     expect(ret.httpVersion).toBe('HTTP/2.0');
     expect(JSON.parse(ret.data)).toBeDefined();
 });
+
+describe('Compressions', () => {
+    test('GZip', async () => {
+        let ret = await client.request('https://httpbin.org/gzip');
+
+        expect(ret.status).toBe(200);
+    });
+
+    test('Brotli', async () => {
+        let ret = await client.request('https://httpbin.org/brotli');
+
+        expect(ret.status).toBe(200);
+    });
+});
+
+describe('Request methods', () => {
+    let methods = ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'];
+
+    for (const method of methods) {
+        test(method, async () => {
+            let ret = await client.request(`https://httpbin.org/${method.toLowerCase()}`, {
+                method,
+            });
+
+            expect(ret.status).toBe(200);
+        });
+    }
+});
+
+test('Request headers', async () => {
+    let ret = await client.request('https://httpbin.org/headers', {
+        headers: {
+            foo: 'bar',
+            lemon: 'strawberry',
+        },
+    });
+
+    expect(ret.status).toBe(200);
+
+    let body = JSON.parse(ret.data);
+
+    expect(body.headers.Foo).toBe('bar');
+    expect(body.headers.Lemon).toBe('strawberry');
+});
+
