be0f6be93d8916997b1d035ff558d92454d14de8
rumblefrog <contact@zhenyangli.me>
2021-08-16 13:39:02 -0400

    test - fb graph server

---
+++ tests/platforms.test.js
@@ -0,0 +1,29 @@
+const { Client, LogLevel } = require('../dist');
+
+jest.setTimeout(10e3);
+
+let client;
+
+beforeAll(()  => {
+    client = new Client({
+        connectTimeout: 5,
+        requestTimeout: 5,
+        redirectLimit: 2,
+        httpsOnly: true,
+        https2AdaptiveWindow: true,
+        logLevel: LogLevel.Debug,
+        stripSensitiveHeaders: false,
+    });
+});
+
+// http2 error: protocol error: unspecific protocol error detected
+test('Facebook graph server', async () => {
+    let ret = await client.request('https://b-graph.facebook.com', {
+        headers: {
+            'Authorization': 'foo',
+            'bar': 'foo',
+        }
+    });
+
+    expect(ret.statusCode).toBe(400);
+});
