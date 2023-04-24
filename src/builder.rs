031e6ecf1fddb998681791371589b66318b4c5df
1Conan <me@1conan.com>
2023-04-24 23:01:15 +0800

    replicate chrome ciphers as close as possible

--- src/builder.rs
+++ src/builder.rs
@@ -200,38 +200,32 @@ impl Builder {
         cb.client = cb.client.use_preconfigured_tls({
             let mut config = ClientConfig::builder()
                 .with_cipher_suites(&[
+                    // GREASE
                     rustls::cipher_suite::TLS13_AES_128_GCM_SHA256,
                     rustls::cipher_suite::TLS13_AES_256_GCM_SHA384,
                     rustls::cipher_suite::TLS13_CHACHA20_POLY1305_SHA256,
-                    rustls::cipher_suite::TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
                     rustls::cipher_suite::TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-                    rustls::cipher_suite::TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
-                    rustls::cipher_suite::TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
                     rustls::cipher_suite::TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+                    rustls::cipher_suite::TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+                    rustls::cipher_suite::TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+                    rustls::cipher_suite::TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
                     rustls::cipher_suite::TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
                     // unsupported
-                    // rustls::cipher_suite::TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
-                    // rustls::cipher_suite::TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
-                    // rustls::cipher_suite::TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
                     // rustls::cipher_suite::TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
-                    // rustls::cipher_suite::TLS_RSA_WITH_AES_256_GCM_SHA384,
+                    // rustls::cipher_suite::TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
                     // rustls::cipher_suite::TLS_RSA_WITH_AES_128_GCM_SHA256,
-                    // rustls::cipher_suite::TLS_RSA_WITH_AES_256_CBC_SHA,
+                    // rustls::cipher_suite::TLS_RSA_WITH_AES_256_GCM_SHA384,
                     // rustls::cipher_suite::TLS_RSA_WITH_AES_128_CBC_SHA,
-                    // rustls::cipher_suite::TLS_ECDHE_ECDSA_WITH_3DES_EDE_CBC_SHA,
-                    // rustls::cipher_suite::TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
-                    // rustls::cipher_suite::TLS_RSA_WITH_3DES_EDE_CBC_SHA,
+                    // rustls::cipher_suite::TLS_RSA_WITH_AES_256_CBC_SHA,
                 ])
                 .with_kx_groups(&[
+                    // GREASE
                     &rustls::kx_group::X25519,
                     &rustls::kx_group::SECP256R1,
                     &rustls::kx_group::SECP384R1,
-                    // &rustls::kx_group::SECP521R1, // unsupported
                 ])
                 .with_protocol_versions(&[
-                    // unsupported
-                    // &rustls::version::TLS10,
-                    // &rustls::version::TLS11,
+                    // GREASE
                     &rustls::version::TLS12,
                     &rustls::version::TLS13,
                 ])
